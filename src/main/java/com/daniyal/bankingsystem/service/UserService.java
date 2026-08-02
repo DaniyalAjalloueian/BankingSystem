bfcb9c406f4316f18fdd1df243bad2656649ca25
dani <daniyalajalloueian@gmail.com>
2026-08-02 16:30:04 +0330

    added findbyid in user service and added UserNotFound exeption

--- src/main/java/com/daniyal/bankingsystem/service/UserService.java
+++ src/main/java/com/daniyal/bankingsystem/service/UserService.java
@@ -1,11 +1,14 @@
 package com.daniyal.bankingsystem.service;
 
+import com.daniyal.bankingsystem.exception.UserNotFound;
 import com.daniyal.bankingsystem.mapper.ResponseUserMapper;
+import com.daniyal.bankingsystem.model.User;
 import com.daniyal.bankingsystem.repository.UserRepository;
 import com.daniyal.bankingsystem.responseDto.ResponseUserDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,5 +22,10 @@ public class UserService {
 
     public List<ResponseUserDto> findAll(){
         return userRepository.findAll().stream().map(responseUserMapper::toDto).toList();
+    }
+
+    public ResponseUserDto findById(long id){
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFound("User not found with id: " + id));
+        return responseUserMapper.toDto(user);
     }
 }
