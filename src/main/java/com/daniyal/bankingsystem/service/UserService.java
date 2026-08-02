aa5a63169ce604162477502adbfdc7273c5fc625
dani <daniyalajalloueian@gmail.com>
2026-08-02 16:14:57 +0330

    added public List<ResponseUserDto> findAll() to userservice

--- src/main/java/com/daniyal/bankingsystem/service/UserService.java
+++ src/main/java/com/daniyal/bankingsystem/service/UserService.java
@@ -1,13 +1,23 @@
 package com.daniyal.bankingsystem.service;
 
+import com.daniyal.bankingsystem.mapper.ResponseUserMapper;
 import com.daniyal.bankingsystem.repository.UserRepository;
+import com.daniyal.bankingsystem.responseDto.ResponseUserDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ResponseUserMapper responseUserMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ResponseUserMapper responseUserMapper) {
         this.userRepository = userRepository;
+        this.responseUserMapper = responseUserMapper;
+    }
+
+    public List<ResponseUserDto> findAll(){
+        return userRepository.findAll().stream().map(responseUserMapper::toDto).toList();
     }
 }
