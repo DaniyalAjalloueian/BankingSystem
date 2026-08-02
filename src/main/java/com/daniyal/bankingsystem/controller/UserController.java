2486c3d7df0a7111fc52bebdbfca61e2b85954a9
dani <daniyalajalloueian@gmail.com>
2026-08-02 16:25:22 +0330

    added find all users in user contoller

--- src/main/java/com/daniyal/bankingsystem/controller/UserController.java
+++ src/main/java/com/daniyal/bankingsystem/controller/UserController.java
@@ -1,9 +1,24 @@
 package com.daniyal.bankingsystem.controller;
 
+import com.daniyal.bankingsystem.responseDto.ResponseUserDto;
+import com.daniyal.bankingsystem.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<ResponseUserDto> findAllUsers(){
+        return userService.findAll();
+    }
 }
