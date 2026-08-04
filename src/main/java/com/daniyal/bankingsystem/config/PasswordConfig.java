a603346e2c4599f9e841a43e9a7d58e8e0a424e2
dani <daniyalajalloueian@gmail.com>
2026-08-04 14:58:49 +0330

    added api key auth

---
+++ src/main/java/com/daniyal/bankingsystem/config/PasswordConfig.java
@@ -0,0 +1,15 @@
+package com.daniyal.bankingsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/daniyal/bankingsystem/security/AuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.daniyal.bankingsystem.security;
+
+import com.daniyal.bankingsystem.service.ApiKeyService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final ApiKeyService apiKeyService;
+
+    public AuthenticationFilter(ApiKeyService apiKeyService) {
+        this.apiKeyService = apiKeyService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String path = request.getServletPath();
+
+        if (path.equals("/api/auth/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String apiKey = request.getHeader("X-API-KEY");
+
+        if (apiKey == null || !apiKeyService.isValid(apiKey)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(
+                        "api-user",
+                        null,
+                        List.of()
+                );
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
